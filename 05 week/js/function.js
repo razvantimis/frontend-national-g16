baa85c2dbff5b5ce3627a8206537a6eb913a80ae
Razvan Timis <razvan.timis@8x8.com>
2021-10-11 19:33:34 +0300

    Add

---
+++ 05 week/js/function.js
@@ -0,0 +1,42 @@
+// 6. Function basic
+
+// f(x) = x + 1
+// f(2) = 3
+// f(4) = 5
+
+// definim functia folosind function urmat de numele ei
+// iar intre paranteze punem parametri
+// {} este corpul functiei
+// By default o functie returneaza undefined
+// "return" ne ajuta sa returnam o valoare calculata din functie
+// Odata ce se executa "return" , se termina executia functie, codul de dupa nu se mai executa
+function f(x) {
+  console.log("inaite x=", x);
+  x = x + 1;
+  console.log("dupa x=", x);
+  return x;
+}
+console.log("f(2) = ", f(2))
+
+
+// afiseaza un mesaj sau erroare
+// o functie ne ajuta sa refolosim codul
+function log(type, msg) {
+  console.log("type=", type);
+  console.log("msg=", msg);
+
+  switch (type) {
+    case "debug":
+      console.log("[debug]:", msg)
+      break;
+    case "error":
+      console.error("[error]:", msg)
+  }
+}
+
+log("debug", "User a dat click pe login button")
+log("error", "Nu merge logarea")
+log("error", "Nu merge logarea")
+log("error", "Nu merge logarea")
+log("error", "Nu merge logarea")
+log("error", "Nu merge logarea")
--- 06 week/readme.md
+++ 06 week/readme.md
@@ -2,6 +2,7 @@
 
 1. Function
     1. Paramater by reference / by value
+    2. == vs ===
 2. Expression function / anonymous function - callback
 2. Scopes - Hoisting ( var vs let/const )
 3. Recursive functions
