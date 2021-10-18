af0a13c3d67ee200110e5e49b24e607036a3ad5e
Razvan Timis <razvan.timis@8x8.com>
2021-10-18 20:46:02 +0300

    Add

--- 06 week/js/hoisting.js
+++ 06 week/js/hoisting.js
@@ -3,24 +3,33 @@
 // Hoisting - duce declaratile de variabile pe prima linie
 // Ca si rezultat, putem folosi variabila oriunde in scopul ei, indiferet ca am definito la final
 
-
 console.log(myName) // undefined
 
-var myName = "Marius";
+var myName = "Marius1";
+var myName = "Marius2";
+var myName = "Marius3";
+var myName = "Marius4";
+var myName = "Marius5";
 
-console.log(myName); // Marius
+console.log(myName); // Marius 5
 
 show();
 
-function show(){
+function show() { // var are scope function / local scope
+  // var age ( face el automat )
   console.log(age) // undefined
+  console.log(myName) // undefined
   age = 10;
-   
-  console.log(age) // age = 
-
-
+  console.log(age) // age = 10
 
   var age = 13;
   console.log(age) // age = 13
 
+  if (age < 18) { // for, while
+    var myName;
+  }
+
+  if (age < 11) {
+    var myName;
+  }
 }
