d790e2e49dc7b109b466c744cf6c6a7213fbada3
StefanRadusi <radusi.stefan@gmail.com>
2021-11-10 20:09:21 +0100

    finish

--- 09 week/script.js
+++ 09 week/script.js
@@ -1,19 +1,33 @@
 console.log('Week 9');
 
 // render initial users
-
 getUsers();
+
+// update list every second
+setInterval(getUsers, 1000);
 
 function renderListItem(userData) {
     const li = document.createElement('li');
     li.innerText = `${userData.last_name} ${userData.first_name}`;
     const ol = document.getElementById('entries');
     ol.appendChild(li);
+
+    const deleteElement = document.createElement('span');
+    deleteElement.innerText = ' x';
+    li.appendChild(deleteElement);
+
+    // delete user
+
+    deleteElement.addEventListener('click', function() {
+        deleteUser(userData.id);
+    });
 }
 
 function renderUsers(data) {
+    document.getElementById('entries').innerHTML = '';
+
     for (const user of data) {
-        console.log(user);
+        // console.log(user);
         renderListItem(user);
     }
 }
@@ -30,15 +44,10 @@ function getUsers() {
 
 // add new user
 
-
-function getNewUserData(data) {
-    console.log(data);
-}
-
 function sendNewUser() {
     const newUser = {
-        first_name: 'Ion',
-        last_name: 'Pop'
+        first_name: document.getElementById('first_name').value,
+        last_name: document.getElementById('last_name').value
     };
 
     console.log(newUser, JSON.stringify(newUser));
@@ -52,7 +61,16 @@ function sendNewUser() {
         body: JSON.stringify(newUser) 
     })
         .then(processRespose)
-        .then(getNewUserData)
+        .then(renderListItem)
 }
 
 document.getElementById('send').addEventListener('click', sendNewUser);
+
+// delete element server call
+
+function deleteUser(userId) {
+    console.log(userId);
+    fetch('https://contact-agenda-rest-api.herokuapp.com/users/' + userId, {
+        method: "DELETE",
+    }); 
+}
